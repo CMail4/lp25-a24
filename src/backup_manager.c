abb2915c5b9b41548d3ad156ddb8da6eb4875ba7
CMail4 <maillardcamil@gmail.com>
2024-11-16 16:55:44 +0100

    Update backup_manager.c
    J'ai rajouté une meilleur explication

--- src/backup_manager.c
+++ src/backup_manager.c
@@ -1,14 +1,16 @@
 #include "backup_manager.h"
 
 void create_backup(const char *source, const char *destination) {
-    // Implémenter la logique de création d'une sauvegarde
+    /*backup commence par effectuer une copie complète de la dernière sauvegarde, par liens durs, avec la date et l'heure actuelles comme nom, sous le format "YYYY-MM-DD-hh:mm:ss.sss"*/
 }
 
 void restore_backup(const char *backup_id, const char *destination) {
-    // Implémenter la logique de restauration d'une sauvegarde
+    /*L'option --restore permet de restaurer une sauvegarde à partir d'un chemin spécifié, que ce soit localement ou depuis un serveur distant.
+    La restauration peut être effectuée en utilisant les informations sur la sauvegarde disponible dans le répertoire de destination ou à travers une connexion réseau.*/
 }
 
 void list_backup(const char *directory) {
-    // Implémenter la logique de liste des sauvegardes
+    /*L'option --list-backups permet d'afficher toutes les sauvegardes existantes, 
+    que ce soit localement ou sur un serveur distant. Cette fonctionnalité est utile pour que l'utilisateur puisse voir toutes les sauvegardes disponibles et décider laquelle restaurer.*/
 }
 
